cdf39430ba313c76296a10b58f6aab544006b37c
Syed Hussain Ather <shussainather@gmail.com>
2023-05-14 19:11:35 -0400

    init

---
+++ cameramovement.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform target;
+    public float cameraDistance = 10.0f;
+    public float cameraHeight = 2.0f;
+    public float cameraSpeed = 3.0f;
+
+    private Vector3 offset;
+
+    private void Start()
+    {
+        offset = new Vector3(0.0f, cameraHeight, -cameraDistance);
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 desiredPosition = target.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, cameraSpeed * Time.deltaTime);
+        transform.position = smoothedPosition;
+
+        transform.LookAt(target);
+    }
+}
+
